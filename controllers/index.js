9abb396d8dbea8b2d27dad907c86ccacf26edb71
terygasmen <terygasmen@gmail.com>
2023-04-22 17:31:06 +0800

    l02 connected to mongodb and showing contacts

--- controllers/index.js
+++
@@ -1,9 +0,0 @@
-const awesomeFunction = (req, res, next) => {
-        res.json('Aurelia Gasmen');
-};
-
-const returnAnotherPerson = (req, res, next) => {
-        res.json('Therese Lee Gasmen');
-};
-
-module.exports = { awesomeFunction, returnAnotherPerson };
--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-const port = process.env.PORT || 3000
-
-app.use('/', require('./routes/index'));
-
-app.listen(3000, () => {
-        console.log(`Server is running on ${port}`);
-});
