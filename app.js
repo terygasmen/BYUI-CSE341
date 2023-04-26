34ed46901833f93c9ded1d959aeb5e43bf002abd
terygasmen <terygasmen@gmail.com>
2023-04-26 12:22:08 +0800

    get, post, put and delete routes working on dev

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 app
-  .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
+    .use(bodyParser.json())
+    .use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      next();
   })
   .use('/', require('./routes/index'));
 
